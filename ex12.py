06980a646a4b15a11fc8753dbd7e7ca75ce0422d
JessicaPortilio <jessicaket91@gmail.com>
2020-07-13 17:07:44 -0300

    first commit

---
+++ ex12.py
@@ -0,0 +1,4 @@
+altura = int(input("Entre com a altura do retângulo: "))
+largura = int(input("Entre com a largura do retângulo: "))
+area = altura * largura
+print("A área do retângula de altura " + str(altura) + " e largura " + str(largura) + " é igual a " + str(area))
